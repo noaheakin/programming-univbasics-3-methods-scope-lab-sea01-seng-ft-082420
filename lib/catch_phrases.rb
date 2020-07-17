8b961b31ab460cc152a6012836fc8b3c0054a0f8
Noah Eakin <noaheakin@gmail.com>
2020-07-17 15:14:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,10 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link
+  warning = "It's Dangerous To Go Alone! Take This."
+  puts warning
